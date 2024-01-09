8ab8cb5327f467b30418cab6cc12e459a86e695b
Jonaira Manha <abcd@Jonaeath.local>
2024-01-10 01:20:20 +0600

    refactor:implement token bearer

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import {createUserWithEmailAndPassword,GoogleAuthProvider,signInWithPopup, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut, updateProfile} from "firebase/auth"
 import app from '../firebase/firebase.config';
+import axios from "axios";
 // import axios from "axios";
 
 export const AuthContext = createContext(null)
@@ -44,6 +45,19 @@ const AuthProvider = ({ children }) => {
         const unsubscribe = onAuthStateChanged(auth, currentUser => {
             setUser(currentUser);
             console.log('current user', currentUser);
+
+            // get and set token
+            if(currentUser){
+                axios.post('http://localhost:4000/jwt', {email: currentUser.email})
+                .then(data=>{
+                    // console.log(data.data.token)
+                    localStorage.setItem('access-token',data.data.token)
+                })
+            }
+            else{
+                localStorage.removeItem('access-token')
+            }
+
             setLoading(false)
             
         });
--- src/hooks/useCart.jsx
+++ src/hooks/useCart.jsx
@@ -7,11 +7,15 @@ import { useQuery } from "react-query";
 
 const useCart = () => {
     const {user} = useContext(AuthContext);
+    const token = localStorage.getItem('access-token')
 
     const {refetch, data: cart = []} = useQuery({
         queryKey: ['carts', user?.email],
         queryFn: async () =>{
-            const res = await fetch(`http://localhost:4000/carts?email=${user?.email}`)
+            const res = await fetch(`http://localhost:4000/carts?email=${user?.email}`, 
+            { headers:{
+                authorization: `bearer ${token}`
+            }})
             return res.json();
         }
     })
