b38fe74deea902bdd432444202f5d14fc167da4c
Jonaira Manha <abcd@Jonaeath.local>
2024-01-13 00:11:38 +0600

    feat:Creat use admin hook

--- src/Layout/Dashboard/Dashboard.jsx
+++ src/Layout/Dashboard/Dashboard.jsx
@@ -9,11 +9,14 @@ import {
   FaUsers,
 } from "react-icons/fa";
 import useCart from "../../hooks/useCart";
+import useAdmin from "../../hooks/useAdmin";
 
 const Dashboard = () => {
   const [cart] = useCart();
   // TODO: load data from the server
-  const isAdmin = true;
+  // const isAdmin = true;
+
+  const [isAdmin] = useAdmin()
 
   return (
     <div className="drawer lg:drawer-open">
